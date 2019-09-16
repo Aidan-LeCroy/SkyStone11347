8bfaf17aca229a6aac9eb9b5cdf0444381d42d29
jcdddragon <44683029+jcdddragon@users.noreply.github.com>
2019-09-16 16:39:59 -0400

    Update DriveBase.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
@@ -1,3 +1,29 @@
+//Package
+package org.firstinspires.ftc.teamcode;
+//Imports
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware;
 public class DiffBase {
+
+    // initialize motors
+
+
+    //motor 1 with encoder, top gear left side
+
+    //motor 2 with encoder, bottom gear left side
+
+    //motor 3 with encoder, top gear right side
+
+    //motor 4 with encoder, bottom gear right side
+
+
+public DiffBase(){
+
+}
 
 }
