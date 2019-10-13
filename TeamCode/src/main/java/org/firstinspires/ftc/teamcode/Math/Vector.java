e7e6326e8868d9b4218d1ab45ddf92adf1923b24
General-Ganon(Vijay) <54725214+General-Ganon@users.noreply.github.com>
2019-10-12 23:08:41 -0400

    Cleaning up Vector.java
    Added comments, changed self-multiplication to clearer squaring and made a few visibility improvements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Vector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Vector.java
@@ -4,9 +4,9 @@ public class Vector {
     private final double x;
     private final double y;
 
-    public Vector(double x, double y){
-        this.x=x;
-        this.y=y;
+    public Vector(double x, double y){ // initializes a vector with an x and y component
+        this.x = x;
+        this.y = y;
     }
 
     public double getX(){
@@ -16,16 +16,16 @@ public class Vector {
         return y;
     }
     public double getMagnitude(){
-        return Math.sqrt((x*x)+(y*y));
+        return Math.sqrt((x ^ 2)+(y ^ 2)); // returns the magnitude of the vector using the Pythagorean Theorem
     }
     public double getAngle() {
-        return (180/Math.PI)*Math.atan2(y,x);
+        return (180/Math.PI)*Math.atan2(y,x); // returns the angle of the vector on the standard unit circle in radians
     }
     public Vector add(Vector vec){
-        return new Vector(x+vec.getX(),y+vec.getY());
+        return new Vector(x + vec.getX(),y + vec.getY()); // combines two vectors
     }
     public Vector scale(double scaleFactor){
-        return new Vector(getX()*scaleFactor,getY()*scaleFactor);
+        return new Vector(getX() * scaleFactor,getY() * scaleFactor); // scales the vector by a specified factor(aka multiplication)
     }
 
 
