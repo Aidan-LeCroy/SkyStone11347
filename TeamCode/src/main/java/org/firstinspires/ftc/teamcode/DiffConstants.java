4c687f4a733b48d33915729c4e5d6d14e2c41e90
General-Ganon(Vijay) <54725214+General-Ganon@users.noreply.github.com>
2019-11-13 15:48:33 -0500

    oi josuke
    i erased the space between me and my computer screen, and now i'm inside a git commit! ain't that just crazy josuke?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffConstants.java
@@ -6,6 +6,8 @@ import com.acmerobotics.dashboard.config.Config;
 public class DiffConstants {
     public static double HEADCONSTANT = (41888.0/192.0);
     public static double STICK_DEADZONE=.2;
+    public static double MAX_SPEED;
+    public static double AUTO_POWER
 
 
 }
