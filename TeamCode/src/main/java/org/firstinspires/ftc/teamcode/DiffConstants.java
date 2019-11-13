55ba53d75dec6e5485bc586f31690f89e6f9114b
meeper31 <45136713+meeper31@users.noreply.github.com>
2019-11-13 16:28:59 -0500

    Update DiffConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffConstants.java
@@ -7,7 +7,7 @@ public class DiffConstants {
     public static double HEADCONSTANT = (308);
     public static double STICK_DEADZONE=.2;
     public static double MAX_SPEED;
-    public static double AUTO_POWER
+    public static double AUTO_POWER;
 
 
 }
