49aaf1f7b332a181d1d8e6632a34d72eb6101502
General-Ganon(Vijay) <54725214+General-Ganon@users.noreply.github.com>
2019-11-15 16:48:10 -0500

    eventually
    we will need to do stuff with teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/DiffPos.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Math;
+/*
+public class DiffPos {
+    public DiffPos(double xPos, double yPos, double thetaPos, boolean isRadian) {
+        if (!isRadian) {
+            thetaPos = Math.toRadians(thetaPos);
+        }
+        Vector position = new Vector(xPos, yPos);
+        Pose pose = new Pose(position, thetaPos);
+    }
+}
+*/
