f64b94f0bc9580394132685210285ac72c2920c2
meeper31 <45136713+meeper31@users.noreply.github.com>
2019-12-16 16:19:42 -0500

    Update TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Philobots/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Philobots/TeleOp.java
@@ -1,26 +1,20 @@
 package org.firstinspires.ftc.teamcode.Philobots;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
-//import org.firstinspires.ftc.teamcode.Tracking;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Diff Swerve TeleOp", group = "TeleOp")
 public class TeleOp extends OpMode {
     Robot robot;
-//    Tracking track;
+
     //deadband for joysticks
     public double DEADBAND_MAG = 0.1;
     public Vector2d DEADBAND_VEC = new Vector2d(DEADBAND_MAG, DEADBAND_MAG);
-    public boolean willResetIMU = false;
-    private boolean changed=false;
-    private boolean foundationDown=false;
+
+    public boolean willResetIMU = true;
+
     public void init() {
         robot = new Robot(this, false);
-//        track=new Tracking(hardwareMap,telemetry);
-//        track.initializeCamera();
-
-
     }
 
     //allows driver to indicate that the IMU should not be reset
@@ -28,13 +22,12 @@ public class TeleOp extends OpMode {
     //relevant because of field-centric controls
     public void init_loop() {
         if (gamepad1.y) {
-            willResetIMU = true;
-        }
-        if (gamepad1.x){
-            willResetIMU=false;
+            willResetIMU = false;
         }
     }
-    public void start () { if (willResetIMU) robot.initIMU(); }
+    public void start () {
+        if (willResetIMU) robot.initIMU();
+    }
 
 
     public void loop() {
@@ -45,8 +38,7 @@ public class TeleOp extends OpMode {
         robot.intake(-gamepad2.left_stick_y);
 
 
-        //uncomment for live tuning of ROT_ADVANTAGE constant
-        //TODO: After tuning, make ROT_ADVANTAGE final
+//        //uncomment for live tuning of ROT_ADVANTAGE constant
 //        if (gamepad1.b) {
 //            robot.driveController.moduleRight.ROT_ADVANTAGE += 0.01;
 //            robot.driveController.moduleLeft.ROT_ADVANTAGE += 0.01;
@@ -55,14 +47,12 @@ public class TeleOp extends OpMode {
 //            robot.driveController.moduleRight.ROT_ADVANTAGE -= 0.01;
 //            robot.driveController.moduleLeft.ROT_ADVANTAGE -= 0.01;
 //        }
-        telemetry.addData("ROT_ADVANTAGE: ", robot.driveController.moduleLeft.ROT_ADVANTAGE);
+//        telemetry.addData("ROT_ADVANTAGE: ", robot.driveController.moduleLeft.ROT_ADVANTAGE);
 
 
         //to confirm that joysticks are operating properly
         telemetry.addData("Joystick 1", joystick1);
         telemetry.addData("Joystick 2", joystick2);
-//        telemetry.addData("Skystone xpos",track.getSkyStoneX());
-//        telemetry.addData("Skystone ypos",track.getSkyStoneY());
 
         telemetry.update();
     }
