34db9115e75aaf691ba60f97d457927920e1ba65
General-Ganon(Vijay) <54725214+General-Ganon@users.noreply.github.com>
2019-12-29 14:23:43 -0500

    sorry mario
    but your autonomous code is in another folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffConstants.java
@@ -6,7 +6,7 @@ import com.acmerobotics.dashboard.config.Config;
 public class DiffConstants {
     public static double HEADCONSTANT = (308);
     public static double STICK_DEADZONE=.2;
-    public static double MAX_SPEED;
+    public static double MAX_SPEED = 188.976; // centimetres per second
     public static double AUTO_POWER;
 
 
