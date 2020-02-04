9261a4c4886d55b491e5332be84304dc7e01bea8
meeper31 <45136713+meeper31@users.noreply.github.com>
2020-02-04 16:44:31 -0500

    Bad things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircuitRunners/Tracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircuitRunners/Tracking.java
@@ -1,51 +1,51 @@
-package org.firstinspires.ftc.teamcode.CircuitRunners;
-
-import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvWebcam;
-public class Tracking {
-    private HardwareMap hardwareMap;
-    private Telemetry telemetry;
-    private OpenCvWebcam camera;
-    private SkystoneDetector detector;
-    private String pipeline;
-    private static final double DEGREES_PER_PIXEl=60/(Math.sqrt(640^2+480^2));
-    public Tracking(HardwareMap hwmap, Telemetry telemetry){
-        hardwareMap=hwmap;
-        this.telemetry=telemetry;
-    }
-    public void initializeCamera(){
-        detector=new SkystoneDetector();
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier
-                ("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        camera=new OpenCvWebcam
-                (hardwareMap.get(WebcamName.class,"Webcam 1"),cameraMonitorViewId);
-        camera.openCameraDevice();
-        camera.setPipeline(detector);
-
-        camera.startStreaming(640,480,OpenCvCameraRotation.UPRIGHT);
-    }
-
-    public double getSkyStoneX(){
-        return detector.getScreenPosition().x;
-    }
-
-    public double getSkyStoneY() {
-        return detector.getScreenPosition().y;
-    }
-//Computing the angle requires only simple linear interpolation.
-//For example, let's assume a camera with a resolution of 1920x1080 that
-// covers a 45 degree angle of view across the diagonal.
+//package org.firstinspires.ftc.teamcode.CircuitRunners;
 //
-//In this case, sqrt(1920^2 + 1080^2) gives 2292.19 pixels along the diagonal.
-// That means each pixel represents 45/2292.19 = .0153994 degrees.
-// Explanation paraphrased from StackOverflow user Jerry Coffin
-// Purely theoretical btw
-
-
-}
-
+//import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//import org.firstinspires.ftc.robotcore.external.Telemetry;
+//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+//import org.openftc.easyopencv.OpenCvCameraRotation;
+//import org.openftc.easyopencv.OpenCvWebcam;
+//public class Tracking {
+//    private HardwareMap hardwareMap;
+//    private Telemetry telemetry;
+//    private OpenCvWebcam camera;
+//    private SkystoneDetector detector;
+//    private String pipeline;
+//    private static final double DEGREES_PER_PIXEl=60/(Math.sqrt(640^2+480^2));
+//    public Tracking(HardwareMap hwmap, Telemetry telemetry){
+//        hardwareMap=hwmap;
+//        this.telemetry=telemetry;
+//    }
+//    public void initializeCamera(){
+//        detector=new SkystoneDetector();
+//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier
+//                ("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+//        camera=new OpenCvWebcam
+//                (hardwareMap.get(WebcamName.class,"Webcam 1"),cameraMonitorViewId);
+//        camera.openCameraDevice();
+//        camera.setPipeline(detector);
+//
+//        camera.startStreaming(640,480,OpenCvCameraRotation.UPRIGHT);
+//    }
+//
+//    public double getSkyStoneX(){
+//        return detector.getScreenPosition().x;
+//    }
+//
+//    public double getSkyStoneY() {
+//        return detector.getScreenPosition().y;
+//    }
+////Computing the angle requires only simple linear interpolation.
+////For example, let's assume a camera with a resolution of 1920x1080 that
+//// covers a 45 degree angle of view across the diagonal.
+////
+////In this case, sqrt(1920^2 + 1080^2) gives 2292.19 pixels along the diagonal.
+//// That means each pixel represents 45/2292.19 = .0153994 degrees.
+//// Explanation paraphrased from StackOverflow user Jerry Coffin
+//// Purely theoretical btw
+//
+//
+//}
+//
