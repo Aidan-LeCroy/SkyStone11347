099510c07dfe01fc34a6c039fb2deba29508d719
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2020-02-19 15:01:25 -0500

    Auto things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircuitRunners/Auton/AutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircuitRunners/Auton/AutoRed.java
@@ -19,6 +19,7 @@ public class AutoRed extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+
         intake = new IntakeSubsystem(this);
         lift = new LiftSubsystem(this);
         vision = new VisionSubsystem(this);
@@ -30,6 +31,22 @@ public class AutoRed extends LinearOpMode {
         intake.initialize();
         lift.initialize();
         vision.initialize();
+        drive.initialize();
+
+        //For intake
+        IntakeCommand intakeCommand = new IntakeCommand(intake, IntakeSubsystem.Direction.IN);
+
+        //For grabber
+        GrabberCommand grabberCommand = new GrabberCommand(lift, GrabberCommand.GrabberPos.OPEN);
+
+        //For 4bar
+        Move4BCommand move4BCommand = new Move4BCommand(lift, Move4BCommand.v4BPos.IN);
+
+        //Things to be moved on init
+        addLog("Moving grabber to init position...");
+        telemetry.update();
+        grabberCommand.initialize(); //Grabber init pos
+        doAndEnd(grabberCommand);
 
         addLog("Initialized...");
         addLog("Starting Vision...");
@@ -49,21 +66,111 @@ public class AutoRed extends LinearOpMode {
 
 
         //Move intake out after sizing
-        new IntakeCommand(intake, IntakeSubsystem.Direction.IN).execute();
+
+        intakeCommand.initialize();
+        intakeCommand.execute();
         sleep(500);
-        new IntakeCommand(intake, IntakeSubsystem.Direction.STOP).execute();
+        intakeCommand.end();
+
+        //No matter where the stone is, the grabber is going to close. Might as well set it up
+        grabberCommand = new GrabberCommand(lift, GrabberCommand.GrabberPos.CLOSE);
+
+        switch(skystonePos){
+            case 0: //Left
+                //Drive to get there
+
+                //Start up intake
+                intakeCommand.initialize();
+                intakeCommand.execute();
+
+                //Drive forward to intake stone
 
 
+                sleep(500); //Wait a second to ensure stone is in
+                intakeCommand.end();
 
+                //Grab stone in preparation
+                grabberCommand.initialize();
+                doAndEnd(grabberCommand);
+
+                //Drive under bridge (maybe to foundation)
+                break;
+            case 1: //Center
+                //Drive to get there
+
+                //Start up intake
+                intakeCommand.initialize();
+                intakeCommand.execute();
+
+                //Drive forward to intake stone
+
+
+                sleep(500); //Wait a second to ensure stone is in
+                intakeCommand.end();
+
+                //Grab stone in preparation
+                grabberCommand.initialize();
+                doAndEnd(grabberCommand);
+
+                //Drive under bridge (maybe to foundation)
+
+                break;
+            case 2: //Right
+                //Drive to get there
+
+                //Start up intake
+                intakeCommand.initialize();
+                intakeCommand.execute();
+
+                //Drive forward to intake stone
+
+
+                sleep(500); //Wait a second to ensure stone is in
+                intakeCommand.end();
+
+                //Grab stone in preparation
+                grabberCommand.initialize();
+                doAndEnd(grabberCommand);
+
+                //Drive under bridge (maybe to foundation)
+
+                break;
+        }
+
+        //Swing out stone
+        move4BCommand.initialize();
+        doAndEnd(move4BCommand);
+        //Wait a little to ensure that it has finished
+        sleep(500);
+
+        //Drop stone
+        grabberCommand = new GrabberCommand(lift, GrabberCommand.GrabberPos.OPEN);
+        grabberCommand.initialize();
+        doAndEnd(grabberCommand);
+
+        //Move 4bar back inside
+        move4BCommand = new Move4BCommand(lift, Move4BCommand.v4BPos.IN);
+        move4BCommand.initialize();
+        doAndEnd(move4BCommand);
+
+        //Drive back to bridge
+
+
+        //DONE
 
         //AT THE END
         intake.stop();
         lift.stop();
+        drive.stop();
         vision.stop(); //<- Just for continuity
-
     }
 
-    private void addLog(String data) {
+    private void doAndEnd(Command command){
+        command.execute();
+        command.end();
+    }
+
+    private void addLog(String data){
         telemetry.log().add(data);
     }
 }
